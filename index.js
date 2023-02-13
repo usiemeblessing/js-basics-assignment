860a07f408a2cd49f9768589b2ed6daf3bbf8fbb
Usieme <usiemeblessing@gmail.com>
2023-02-13 20:18:00 +0100

    updated index.js file

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-/** @format */
-
-<script>
-	//Function to multiply two numbers function multiplyNumbers(firstNumber,
+	//Function to multiply two numbers 
+	
+	
+	function multiplyNumbers(firstNumber,
 	secondNumber)
-	{/* {
-    return firstNumber*secondNumber;
-  } */}
-	//My Data let firstNumber = 4; let secondNumber = 25; let product =
-	multiplyNumbers(firstNumber, secondNumber); console.log(product); let
-	firstName = "Blessing"; let lastName = "Usieme"; let stuternTrack = 1.4; let
-	skills = "HTML, CSS"; Let favoriteColor = "blue" let yearOfIndependence =
-	1960; console.log(My name is +firstName + lastName, + I am in Stutern Track
+	let firstNumber = 4; 
+	let secondNumber = 25; 
+	let product =firstNumber * secondNumber; 
+	console.log(product);
+
+
+
+	//My Data 
+	
+	let firstName = "Blessing"; 
+	let lastName = "Usieme"; 
+	let stuternTrack = 1.4; 
+	let skills = "HTML, CSS"; 
+	let favoriteColor = "blue";
+	let yearOfIndependence = 1960; 
+	
+	console.log(I am +firstName + lastName, + I am in Stutern Track
 	+ stuternTrack.+ My Skills are + skills + My favorite color is
 	+favoriteColor+ My Country gained her independence in + yearOfIndependence);
-</script>;
